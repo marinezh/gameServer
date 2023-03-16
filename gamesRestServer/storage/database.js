bfb820d89a8daf3e89dfc10b67cf53de8081bea6
Marina <78799773+marinezh@users.noreply.github.com>
2023-03-17 00:33:37 +0200

    add working indexRest.js

--- gamesRestServer/storage/database.js
+++ gamesRestServer/storage/database.js
@@ -1,45 +1,38 @@
-'use strict';
+"use strict";
 
-const mariadb=require('mariadb');
+const mariadb = require("mariadb");
 
-module.exports=class Database{
+module.exports = class Database {
+  constructor(options) {
+    this.options = options;
+  }
 
-    constructor(options){
-        this.options=options;
-    }
-
-    doQuery(sql,parameters){
-        return new Promise(async (resolve,reject)=>{
-            let connection;
-            try{
-                connection = await mariadb.createConnection(this.options);
-                let queryResult = await connection.query(sql,parameters);
-                if(typeof queryResult === 'undefined'){
-                    reject('QueryError');
-                }
-                else if(typeof queryResult.affectedRows==='undefined'){
-                    delete queryResult.meta;
-                    resolve({queryResult,resultSet:true})
-                }
-                else{
-                    resolve({
-                        queryResult:{
-                            rowsChanged: queryResult.affectedRows,
-                            insertId: queryResult.insertId, 
-                            status: queryResult.warningStatus
-                        },
-                        resultSet:false
-                    });
-                }
-            }
-            catch(err){
-                reject('SQL-error: '+err);
-            }
-            finally{
-                if(connection) connection.end();
-            }
-        });
-    }
-
-
-}
+  doQuery(sql, parameters) {
+    return new Promise(async (resolve, reject) => {
+      let connection;
+      try {
+        connection = await mariadb.createConnection(this.options);
+        let queryResult = await connection.query(sql, parameters);
+        if (typeof queryResult === "undefined") {
+          reject("QueryError");
+        } else if (typeof queryResult.affectedRows === "undefined") {
+          //delete queryResult.meta;
+          resolve({ queryResult, resultSet: true });
+        } else {
+          resolve({
+            queryResult: {
+              rowsChanged: queryResult.affectedRows,
+              insertId: queryResult.insertId,
+              status: queryResult.warningStatus,
+            },
+            resultSet: false,
+          });
+        }
+      } catch (err) {
+        reject("SQL-error: " + err);
+      } finally {
+        if (connection) connection.end();
+      }
+    });
+  }
+};
